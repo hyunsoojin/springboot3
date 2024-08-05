4c2902f9361c38d5d1a28bc8ffd03f1caa3c07e1
Jinny <jhs7251@naver.com>
2024-08-05 21:06:01 +0900

    chapter6 update

--- src/main/java/org/example/service/BlogService.java
+++ src/main/java/org/example/service/BlogService.java
@@ -6,6 +6,8 @@ import org.example.dto.AddArticleRequest;
 import org.example.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -13,5 +15,18 @@ public class BlogService {
 
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id){
+        blogRepository.deleteById(id);
     }
 }
